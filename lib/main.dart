540238e6860814887c0b504370bae09e754dd435
fkn1017 <kosuke.fukano@gmail.com>
2021-08-22 20:44:17 +0900

    riverpodを追加。ProviderにBookmarkRepositoryを登録。

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookmark/models/bookmark.dart';
+import 'package:flutter_bookmark/repositories/bookmark_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'home.dart';
 
+late final bookmarkRepository;
+
 void main() async {
   await Hive.initFlutter();
-
-  Hive.registerAdapter(BookmarkAdapter());
+  await _initBox();
 
   runApp(MyApp());
+}
+
+Future<void> _initBox() async {
+  Hive.registerAdapter(BookmarkAdapter());
+  final Box<Bookmark> _box = await Hive.openBox('bookmark');
+  bookmarkRepository = Provider((ref) => BookmarkRepository(_box));
 }
 
 class MyApp extends StatelessWidget {
