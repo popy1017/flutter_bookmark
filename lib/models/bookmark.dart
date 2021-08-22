2dbea5e88a01861da62dae924ad8f5456a9548b0
fkn1017 <kosuke.fukano@gmail.com>
2021-08-22 21:25:46 +0900

    フィールドにidを追加

--- lib/models/bookmark.dart
+++ lib/models/bookmark.dart
@@ -5,17 +5,21 @@ part 'bookmark.g.dart';
 @HiveType(typeId: 0)
 class Bookmark {
   Bookmark({
+    required this.id,
     required this.title,
     required this.description,
     required this.imageUri,
   });
 
   @HiveField(0)
-  String title;
+  String id;
 
   @HiveField(1)
-  String description;
+  String title;
 
   @HiveField(2)
+  String description;
+
+  @HiveField(3)
   String imageUri;
 }
--- lib/models/bookmark.g.dart
+++ lib/models/bookmark.g.dart
@@ -17,21 +17,24 @@ class BookmarkAdapter extends TypeAdapter<Bookmark> {
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
     return Bookmark(
-      title: fields[0] as String,
-      description: fields[1] as String,
-      imageUri: fields[2] as String,
+      id: fields[0] as String,
+      title: fields[1] as String,
+      description: fields[2] as String,
+      imageUri: fields[3] as String,
     );
   }
 
   @override
   void write(BinaryWriter writer, Bookmark obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(4)
       ..writeByte(0)
-      ..write(obj.title)
+      ..write(obj.id)
       ..writeByte(1)
-      ..write(obj.description)
+      ..write(obj.title)
       ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
       ..write(obj.imageUri);
   }
 
