dc3f334cbe712441f3dd9de7bd60398ba19f01a6
fkn1017 <kosuke.fukano@gmail.com>
2021-08-23 22:44:48 +0900

    webページのmetadataを取得する部分を追加

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookmark/components/text_form_dialog.dart';
+import 'package:flutter_bookmark/main.dart';
+import 'package:flutter_bookmark/repositories/bookmark_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:metadata_fetch/metadata_fetch.dart';
+import 'package:uuid/uuid.dart';
 
 import 'components/bookmark_list.dart';
+import 'models/bookmark.dart';
 
 class Home extends ConsumerWidget {
   @override
@@ -18,10 +23,27 @@ class Home extends ConsumerWidget {
           );
 
           if (url != null) {
-            print(url);
+            await _createBookmark(url, ref);
           }
         },
       ),
     );
+  }
+
+  Future<void> _createBookmark(String url, WidgetRef ref) async {
+    final Metadata? metadata = await MetadataFetch.extract(url);
+
+    if (metadata != null) {
+      final BookmarkRepository _bookmarkRepository =
+          ref.read(bookmarkRepository);
+      final Bookmark _newBookmark = Bookmark(
+        id: Uuid().v4(),
+        title: metadata.title ?? '',
+        imageUri: metadata.image ??
+            'https://flutter.dev/images/flutter-logo-sharing.png',
+        description: metadata.description ?? '',
+      );
+      await _bookmarkRepository.create(_newBookmark);
+    }
   }
 }
