231a409ea18c99dc0579c77340dcf8e5bacddafa
fkn1017 <kosuke.fukano@gmail.com>
2021-08-24 20:33:34 +0900

    Bookmarkカードをタップするとそのページが表示されるように修正

---
+++ lib/bookmark_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bookmark/models/bookmark.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class BookmarkView extends StatelessWidget {
+  BookmarkView(this._bookmark);
+
+  final Bookmark _bookmark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_bookmark.title),
+      ),
+      body: WebView(
+        initialUrl: _bookmark.url,
+      ),
+    );
+  }
+}
--- lib/components/bookmark_card.dart
+++ lib/components/bookmark_card.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bookmark/bookmark_view.dart';
 import 'package:flutter_bookmark/models/bookmark.dart';
 
 class BookmarkCard extends StatelessWidget {
-  BookmarkCard(this.bookmark);
+  BookmarkCard(this._bookmark);
 
-  final Bookmark bookmark;
+  final Bookmark _bookmark;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8),
-      child: Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.network(
-              bookmark.imageUri.toString(),
-              fit: BoxFit.fitWidth,
-              width: double.infinity,
-              height: 200,
-            ),
-            ListTile(
-              title: Text(bookmark.title),
-              subtitle: Text(
-                bookmark.description,
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (_) => BookmarkView(_bookmark)));
+      },
+      child: Card(
+        margin: const EdgeInsets.all(8),
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                _bookmark.imageUri.toString(),
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+                height: 200,
               ),
-            ),
-          ],
+              ListTile(
+                title: Text(_bookmark.title),
+                subtitle: Text(
+                  _bookmark.description,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
