e90cfc328d6f1cfe7c71c481faa88189f6d3c0bd
fkn1017 <kosuke.fukano@gmail.com>
2021-08-24 20:05:26 +0900

    スタイルを調整

--- lib/components/bookmark_card.dart
+++ lib/components/bookmark_card.dart
@@ -11,15 +11,24 @@ class BookmarkCard extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.all(8),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(0.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.network(bookmark.imageUri.toString()),
+            Image.network(
+              bookmark.imageUri.toString(),
+              fit: BoxFit.fitWidth,
+              width: double.infinity,
+              height: 200,
+            ),
             ListTile(
               title: Text(bookmark.title),
-              subtitle: Text(bookmark.description),
+              subtitle: Text(
+                bookmark.description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ],
         ),
